3b90c47f8de72b8c79e263393fa72ee6a52c3b2f
saquibmansuri <saquibmansuri@gmail.com>
2024-03-15 18:39:07 +0530

    Adds project file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ValentinesDay
+# Project created for a friend who wanted to impress someone
+
+
+Install python on your system
+Change the name of the couple in heart.py file on line number 18
+Open terminal, go to the directory where heart.py is present, run the project using this command: python heart.py
---
+++ heart.py
@@ -0,0 +1,21 @@
+from turtle import *
+import turtle
+wn=turtle. Screen()
+wn. bgcolor("black")
+color("red")
+begin_fill()
+pensize(5)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+setposition
+turtle.color('white')
+style = ('Courier', 20, 'italic')
+turtle.write('X LOVES Y', font=style, align='center')
+turtle.hideturtle()
+
+
